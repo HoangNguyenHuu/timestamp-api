54e326b83c2ebf5ccad114eb2d9b536910417aa5
Nguyen Huu Hoang <hoangnguyenhuu94@gmail.com>
2016-12-04 10:02:06 +0000

    complete timestamp

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express')
 var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var moment = require("moment")
+app.get('/:query', function (req, res) {
+    var query = req.params.query;
+    var unix = null;
+    var natural = null;
+    if(+query >= 0){
+        console.log(query);
+        unix = +query;
+        natural = moment.unix(unix).format('MMMM D, YYYY');
+    }
+    if(isNaN(query) && moment(query, 'MMMM D, YYYY').isValid()){
+        unix = +moment(query, 'MMMM D, YYYY').format('X');
+        natural = moment.unix(unix).format('MMMM D, YYYY');
+    }
+    
+    var dateObj = {"unix": unix, "natural": natural};
+    res.send(dateObj);
 })
 
 app.listen(8080, function () {
