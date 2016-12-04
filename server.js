68bc485cd655d44b64b67b4119aa28d35b340836
Nguyen Huu Hoang <hoangnguyenhuu94@gmail.com>
2016-12-04 15:55:25 +0000

    add packages moment

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 var moment = require("moment")
+app.get('/', function(req, res){
+   res.send('Hello World!') 
+})
 app.get('/:query', function (req, res) {
     var query = req.params.query;
     var unix = null;
